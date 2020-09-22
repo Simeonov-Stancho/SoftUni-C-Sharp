21b360eaea80e8036897c6ce9549850a8f4358d9
Stancho Simeonov <63386963+Simeonov-Stancho@users.noreply.github.com>
2020-09-22 09:28:32 +0300

    Add files via upload

---
+++ C-Sharp-Advance-SoftUni 2020 09/2020 09 15 Stacks and Queues/2.StackSum/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.StackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> integerStack = new Stack<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                integerStack.Push(numbers[i]);
+            }
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine().ToLower()) != "end")
+            {
+                List<string> currentCommand = input
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                string command = currentCommand[0];
+
+                if (command == "add")
+                {
+                    integerStack.Push(int.Parse(currentCommand[1]));
+                    integerStack.Push(int.Parse(currentCommand[2]));
+                }
+
+                else if (command == "remove")
+                {
+                    int count = int.Parse(currentCommand[1]);
+                    if (count > integerStack.Count)
+                    {
+                        continue;
+                    }
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        integerStack.Pop();
+                    }
+                }
+            }
+
+            int sum = integerStack.Sum();
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
---
+++ C-Sharp-Advance-SoftUni 2020 09/2020 09 15 Stacks and Queues/4.MatchingBrackets/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _4.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string expression = Console.ReadLine();
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char currentChar = expression[i];
+
+                if (currentChar == '(')
+                {
+                    stack.Push(i);
+                }
+
+                else if (currentChar == ')')
+                {
+                    int firstPossition = stack.Pop();
+                    int length = i - firstPossition + 1;
+                    string currentExpression = expression.Substring(firstPossition, length);
+                    Console.WriteLine(currentExpression);
+                }
+            }
+        }
+    }
+}
