c5612d571c06438b768c1e0a3acb8a6787dec274
Stancho Simeonov <63386963+Simeonov-Stancho@users.noreply.github.com>
2020-09-26 23:35:37 +0300

    Add files via upload

---
+++ C-Sharp-Advance-SoftUni 2020 09/2020 09 26 Sets And Dictionaries Advance/07.SoftUniParty/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _07.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            HashSet<string> guests = new HashSet<string>();
+
+            while ((input = Console.ReadLine()) != "PARTY")
+            {
+                guests.Add(input);
+            }
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                guests.Remove(input);
+            }
+
+            Console.WriteLine(guests.Count);
+
+            foreach (var guest in guests)
+            {
+                if (char.IsDigit(guest[0]))
+                {
+                    Console.WriteLine(guest);
+                }
+            }
+
+            foreach (var guest in guests)
+            {
+                if (char.IsLetter(guest[0]))
+                {
+                    Console.WriteLine(guest);
+                }
+            }
+        }
+    }
+}
