7c2a515a8aa9abb2160303c1bf74a9ec426e1aa9
Stancho Simeonov <63386963+Simeonov-Stancho@users.noreply.github.com>
2020-10-03 19:14:07 +0300

    Add files via upload

---
+++ C-Sharp-Advance-SoftUni 2020 09/2020 10 03 Functional Programming/2.SumNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> intFunc = int.Parse;
+
+            var numbers = Console.ReadLine()
+                        .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(intFunc);
+
+            Console.WriteLine(numbers.Count());
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
---
+++ C-Sharp-Advance-SoftUni 2020 09/2020 10 03 Functional Programming/4.AddVAT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(double.Parse)
+                    .Select(p => p * 1.2)
+                    .Select(p => $"{p:f2}")
+                    .ToList()
+                    .ForEach(Console.WriteLine);
+        }
+    }
+}
