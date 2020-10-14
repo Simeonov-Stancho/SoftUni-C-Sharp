77cc6bcc62df50b80db78789099c3eacf8cdab97
Stancho Simeonov <63386963+Simeonov-Stancho@users.noreply.github.com>
2020-10-14 20:48:20 +0300

    Add files via upload

---
+++ C-Sharp-Advance-SoftUni 2020 09/2020 10 25 Exams/2020 08 19 Retake Exam/2020 06 28 Exam/01.2020June28Bombs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._2020June28Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
