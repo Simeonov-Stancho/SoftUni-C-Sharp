ecba61e2af497cf67235bc22eac209ec71a7e8b1
Stancho Simeonov <63386963+Simeonov-Stancho@users.noreply.github.com>
2020-10-20 21:17:35 +0300

    Add files via upload

---
+++ C-Sharp-Advance-SoftUni 2020 09/2020 10 20 Generics/02.ArrayCreator/ArrayCreator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T item)
+        {
+            T[] data = new T[length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = item;
+            }
+
+            return data;
+        }
+    }
+}
