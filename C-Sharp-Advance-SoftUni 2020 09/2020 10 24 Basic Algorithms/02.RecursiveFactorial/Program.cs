4560dad60ef807b1df8ec9d8b610512925bcf3dc
Stancho Simeonov <63386963+Simeonov-Stancho@users.noreply.github.com>
2020-10-24 09:28:45 +0300

    Add files via upload

---
+++ C-Sharp-Advance-SoftUni 2020 09/2020 10 24 Basic Algorithms/02.RecursiveFactorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02.RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factoriel(n));
+        }
+
+        public static int Factoriel(int n)
+        {
+            if (n==1)
+            {
+                return 1;
+            }
+
+            return n * Factoriel(n - 1);
+        }
+    }
+}
