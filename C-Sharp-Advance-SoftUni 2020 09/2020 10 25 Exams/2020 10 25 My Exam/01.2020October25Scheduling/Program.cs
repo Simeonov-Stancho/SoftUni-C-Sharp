700162950e9f4357d045a6c19cadf7f46be43ea6
Stancho Simeonov <63386963+Simeonov-Stancho@users.noreply.github.com>
2020-10-25 18:23:06 +0200

    Add files via upload

--- C-Sharp-Advance-SoftUni 2020 09/2020 10 25 Exams/2020 10 25 My Exam/01.2020October25Scheduling/Program.cs
+++ C-Sharp-Advance-SoftUni 2020 09/2020 10 25 Exams/2020 10 25 My Exam/01.2020October25Scheduling/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace _01._2020October25
+namespace _01.Scheduling
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] tasksArray = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] threadsArray = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int taskValue = int.Parse(Console.ReadLine());
+            Stack<int> tasks = new Stack<int>(tasksArray);
+            Queue<int> threads = new Queue<int>(threadsArray);
+
+            while (taskValue != tasks.Peek() || threads.Count == 0)
+            {
+                if (threads.Peek() >= tasks.Peek())
+                {
+                    threads.Dequeue();
+                    tasks.Pop();
+                }
+
+                else
+                {
+                    threads.Dequeue();
+                }
+            }
+
+            Console.WriteLine($"Thread with value {threads.Peek()} killed task {taskValue}");
+
+            Console.WriteLine(string.Join(" ", threads));
         }
     }
 }
