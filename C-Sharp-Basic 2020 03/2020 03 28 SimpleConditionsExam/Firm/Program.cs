f296c8a670d7a546b1d0a020c7ecb8f9a0ce8520
Stancho Simeonov <63386963+Simeonov-Stancho@users.noreply.github.com>
2020-10-31 08:35:47 +0200

    Add files via upload

---
+++ C-Sharp-Basic 2020 03/2020 03 28 SimpleConditionsExam/Firm/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Firm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //read
+            int prоjectTime = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int warkers = int.Parse(Console.ReadLine());
+
+
+            //10%holliday
+            double warkingDays = days * 0.9;
+
+            //oneDay = 8 hours + overTime = 2 hours
+            double warkingHours = warkingDays * 10 * warkers;
+            //Math.Floor(hours)
+
+            double difference = prоjectTime - warkingHours;
+
+            if (prоjectTime <= warkingHours)
+            {
+                Console.WriteLine($"Yes!{Math.Floor(-difference)} hours left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough time!{Math.Floor(difference)} hours needed.");
+            }
+        }
+    }
+}
---
+++ C-Sharp-Basic 2020 03/2020 03 29 ConditionalStatementsExercise/Cinema/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kindTicket = Console.ReadLine();
+            int r = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            double price = 0;
+            
+            switch (kindTicket)
+            {
+                case "Premiere":
+                price = 12.00;
+                    break;
+
+                case "Normal":
+                    price = 7.50;
+                    break;
+                
+                default:
+                    price = 5.00;
+                   break;
+            }
+
+            Console.WriteLine($"{(price * r * c):f2} leva, income");
+
+        }
+    }
+}
