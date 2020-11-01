550ce2883a27f8aa3623e859bfb45b00ffd6857c
Stancho Simeonov <inj.simeonov@abv.bg>
2020-11-01 18:36:56 +0200

    C-Sharp OOP

---
+++ 2020 10 OOP-C-Sharp/2020 10 31 Inheritance/04.RandomList/RandomList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public Random rand;
+
+        public RandomList()
+        {
+            rand = new Random();
+        }
+
+        public string RandomString()
+        {
+            int index = rand.Next(0, this.Count);
+            string removedElement = this[index];
+            this.RemoveAt(index);
+
+            return removedElement;
+        }
+    }
+}
