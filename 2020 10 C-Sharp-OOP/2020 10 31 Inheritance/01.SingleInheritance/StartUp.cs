1712dd56b01c300bada387cf155b69d1c50271d3
Stancho Simeonov <63386963+Simeonov-Stancho@users.noreply.github.com>
2020-11-01 17:04:56 +0200

    Add files via upload

---
+++ 2020 10 C-Sharp-OOP/2020 10 31 Inheritance/01.SingleInheritance/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog mecho = new Dog();
+            mecho.Eat();
+            mecho.Bark();
+        }
+    }
+}
---
+++ 2020 10 C-Sharp-OOP/2020 10 31 Inheritance/04.RandomList/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("asd");
+            list.Add("bmw");
+            list.Add("audi");
+            list.Add("begai");
+
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
