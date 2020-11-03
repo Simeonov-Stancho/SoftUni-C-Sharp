f4250abc2e6d429dde6f6672f3bbbadd57294435
Stancho Simeonov <inj.simeonov@abv.bg>
2020-11-03 17:40:53 +0200

    Basic

---
+++ 2020 03 29 ConditionalStatementsExercise/On Time for the Exam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace On_Time_for_the_Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
