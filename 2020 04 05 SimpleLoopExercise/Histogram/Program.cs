21c31852faa52cfc0364f37fe33778b40bbbd778
Stancho Simeonov <inj.simeonov@abv.bg>
2020-11-04 06:44:11 +0200

    Basic

---
+++ 2020 04 05 SimpleLoopExercise/Histogram/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Histogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double count = double.Parse(Console.ReadLine());
+            double countNumberP1 = 0;
+            double countNumberP2 = 0;
+            double countNumberP3 = 0;
+            double countNumberP4 = 0;
+            double countNumberP5 = 0;
+
+            for (int i = 1; i <= count; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number < 200)
+                {
+                    countNumberP1 += 1;
+                }
+                else if (number >= 200 && number <= 399)
+                {
+                    countNumberP2 += 1;
+                }
+                else if (number >= 400 && number <= 599)
+                {
+                    countNumberP3 += 1;
+                }
+                else if (number >= 600 && number <= 799)
+                {
+                    countNumberP4 += 1;
+                }
+                else
+                {
+                    countNumberP5 += 1;
+                }
+            }
+            double countNumber = countNumberP1 + countNumberP2 + countNumberP3 + countNumberP4 + countNumberP5;
+            Console.WriteLine($"{(countNumberP1 / countNumber * 100):f2}%");
+            Console.WriteLine($"{(countNumberP2 / countNumber * 100):f2}%");
+            Console.WriteLine($"{(countNumberP3 / countNumber * 100):f2}%");
+            Console.WriteLine($"{(countNumberP4 / countNumber * 100):f2}%");
+            Console.WriteLine($"{(countNumberP5 / countNumber * 100):f2}%");
+
+        }
+    }
+}
---
+++ 2020 04 05 SimpleLoopExercise/Test/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Hristogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double under200Counter = 0;
+            double between200And399Counter = 0;
+            double between400And599Counter = 0;
+            double between600And799Counter = 0;
+            double above800Counter = 0;
+            double p1 = 0.0;
+            double p2 = 0.0;
+            double p3 = 0.0;
+            double p4 = 0.0;
+            double p5 = 0.0;
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number < 200)
+                {
+                    under200Counter += 1;
+                }
+                else if (200 <= number && number <= 399)
+                {
+                    between200And399Counter += 1;
+                }
+                else if (400 <= number && number <= 599)
+                {
+                    between400And599Counter = 1;
+                }
+                else if (600 <= number && number <= 799)
+                {
+                    between600And799Counter += 1;
+                }
+                else if (number >= 800)
+                {
+                    above800Counter += 1;
+                }
+            }
+            p1 = (under200Counter / n) * 100;
+            p2 = (between200And399Counter / n) * 100;
+            p3 = (between400And599Counter / n) * 100;
+            p4 = (between600And799Counter / n) * 100;
+            p5 = (above800Counter / n) * 100;
+            Console.WriteLine($"{p1:f2}%");
+            Console.WriteLine($"{p2:f2}%");
+            Console.WriteLine($"{p3:f2}%");
+            Console.WriteLine($"{p4:f2}%");
+            Console.WriteLine($"{p5:f2}%");
+        }
+    }
+}
