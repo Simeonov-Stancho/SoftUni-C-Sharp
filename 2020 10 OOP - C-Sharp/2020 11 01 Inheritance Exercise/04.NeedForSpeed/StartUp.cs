6b7def02096886c413f1bd59d25585586101ac99
Stancho Simeonov <inj.simeonov@abv.bg>
2020-11-06 13:41:13 +0200

    OOP

---
+++ 2020 10 OOP - C-Sharp/2020 11 01 Inheritance Exercise/04.NeedForSpeed/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SportCar bmw = new SportCar(555, 50.5);
+            bmw.Drive(10);
+            Console.WriteLine(bmw.Fuel);
+        }
+    }
+}
