b0a94417671af867fe6860fb243b54b0e15c49fc
Stancho Simeonov <63386963+Simeonov-Stancho@users.noreply.github.com>
2020-12-06 15:53:47 +0200

    OOP

---
+++ 2020 10 OOP - C-Sharp/2020 12 06 Reflaction and Attributes Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string COMMAND_POSTFIX = "Command";
+
+        public CommandInterpreter()
+        {
+
+        }
+         
+        public string Read(string args)
+        {
+            string[] inputArgs = args
+                                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                 .ToArray();
+
+            string commandName = inputArgs[0] + COMMAND_POSTFIX;
+            string[] commandArgs = inputArgs.Skip(1).ToArray();
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+            Type commandType = assembly
+                    .GetTypes()
+                    .FirstOrDefault(t => t.Name.ToLower() == commandName.ToLower());
+
+            if (commandType == null)
+            {
+                throw new ArgumentException("Invalid command type!");
+            }
+
+            ICommand commandInstance = (ICommand)Activator.CreateInstance(commandType);
+
+            string result = commandInstance.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
