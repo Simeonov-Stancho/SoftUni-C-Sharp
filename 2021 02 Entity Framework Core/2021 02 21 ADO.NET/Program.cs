1904e003c8f6e04aba9fd02d26247044a8ea6ba9
Stancho Simeonov <63386963+Simeonov-Stancho@users.noreply.github.com>
2021-02-22 22:37:22 +0200

    EF

---
+++ 2021 02 Entity Framework Core/2021 02 21 ADO.NET/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace CSharpDbDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection
+                        ("Server=.;Integrated Security=true;Database=SoftUni"))
+            {
+                connection.Open();
+
+                string query = "SELECT (*) FROM Employees";
+                SqlCommand sqlCommand = new SqlCommand(query, connection);
+
+                sqlCommand.ExecuteNonQuery();
+            }
+
+
+        }
+    }
+}
