8bffef86756247a0a010ce2335d9709b269d344d
Stancho Simeonov <63386963+Simeonov-Stancho@users.noreply.github.com>
2021-03-11 22:35:48 +0200

    EF

---
+++ 2021 02 Entity Framework Core/2021 03 09 Entity Relations Exercise/P03_FootballBettingInDifferentLayers/P03_FootballBetting.Data/Migrations/20210311200917_CorrectingNames.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P03_FootballBetting.Migrations
+{
+    public partial class CorrectingNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LogogUrl",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "Amaunt",
+                table: "Bets");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LogoUrl",
+                table: "Teams",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Amount",
+                table: "Bets",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LogoUrl",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Bets");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LogogUrl",
+                table: "Teams",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Amaunt",
+                table: "Bets",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
