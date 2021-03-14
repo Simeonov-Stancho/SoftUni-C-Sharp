0dd19c4a364d1a71b4f400d06f3443725ee0adec
Stancho Simeonov <63386963+Simeonov-Stancho@users.noreply.github.com>
2021-03-14 10:18:30 +0200

    EF

---
+++ 2021 02 Entity Framework Core/2021 03 14 Linq Exercise/MusicHub/Data/Models/Enums/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicHub.Enumerators
+{
+    public enum Genre
+    {
+        Blues = 0,
+        Rap = 1,
+        PopMusic = 2,
+        Rock = 3,
+        Jazz = 4
+    }
+}
