3bd050f680f65645b36691734f324f5b1aa956a0
Stancho Simeonov <63386963+Simeonov-Stancho@users.noreply.github.com>
2021-03-28 10:58:34 +0300

    EF

---
+++ 2021 02 Entity Framework Core/2021 03 21 Json Processing Exercise/16. Export Local Suppliers/CarDealerProfile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using AutoMapper;
+using CarDealer.DTO;
+using CarDealer.DTO.ResultsDto;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            this.CreateMap<SupplierInputModelDto, Supplier>();
+
+            this.CreateMap<PartInputModelDto, Part>();
+
+            this.CreateMap<CustomerInputModelDto, Customer>();
+
+            this.CreateMap<SaleInputModelDto, Sale>();
+
+            this.CreateMap<Customer, CarOutputModelDto>();
+
+            this.CreateMap<CarOutputModelDto, Customer>();
+        }
+    }
+}
