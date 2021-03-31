96e6b50e91c54e01aae3bb3af93960686909f5e2
Stancho Simeonov <63386963+Simeonov-Stancho@users.noreply.github.com>
2021-03-31 18:26:01 +0300

    EF

---
+++ 2021 02 Entity Framework Core/2021 03 28 XML Processing Exercise/01. Import Users/StartUp.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using ProductShop.Data;
+using ProductShop.Dtos.Import;
+using ProductShop.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        private static string DatasetsDirectoryPath = "../../../Datasets";
+        private static string ResultDirectoryPath = "../../../Datasets/Results/";
+
+        public static void Main(string[] args)
+        {
+            var db = new ProductShopContext();
+
+            //01. Import Users
+            var inputUsersXml = File.ReadAllText($"{DatasetsDirectoryPath}/users.xml");
+            Console.WriteLine(ImportUsers(db, inputUsersXml));
+
+            //02. Import Products
+
+            //03. Import Categories
+
+            //04. Import Categories and Products
+
+            //05. Export Products In Range
+
+            //06. Export Sold Products
+
+            //07. Export Categories By Products Count
+
+            //08. Export Users and Products
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            InitializeMapper();
+
+            var xmlRoot = new XmlRootAttribute("Users");
+            var xmlSerializer = new XmlSerializer(typeof(List<ImportUserDto>), xmlRoot);
+
+            var usersDto = (List<ImportUserDto>)xmlSerializer.Deserialize(new StringReader(inputXml));
+
+            var users = Mapper.Map<List<User>>(usersDto);
+
+            context.Users.AddRange(users);
+
+            return $"Successfully imported {context.SaveChanges()}";
+        }
+
+        public static void ResetDatabese(ProductShopContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+
+        private static void InitializeMapper()
+        {
+            Mapper.Initialize(cfg => { cfg.AddProfile<ProductShopProfile>(); });
+        }
+
+    }
+}
