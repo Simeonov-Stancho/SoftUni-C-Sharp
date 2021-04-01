3744d76096dcd870a7116a851c7e7287a05fe97c
Stancho Simeonov <63386963+Simeonov-Stancho@users.noreply.github.com>
2021-04-01 08:16:54 +0300

    EF

---
+++ 2021 02 Entity Framework Core/2021 03 28 XML Processing Exercise/04. Import Categories and Products/StartUp.cs
@@ -0,0 +1,193 @@
+﻿using AutoMapper;
+using ProductShop.Data;
+using ProductShop.Dtos.Import;
+using ProductShop.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        private static string DatasetsDirectoryPath = "../../../Datasets";
+        private static string ResultDirectoryPath = "../../../Datasets/Results/";
+
+        public static void Main(string[] args)
+        {
+            var db = new ProductShopContext();
+            InitializeMapper();
+
+            //01. Import Users
+            //var inputUsersXml = File.ReadAllText($"{DatasetsDirectoryPath}/users.xml");
+            //Console.WriteLine(ImportUsers(db, inputUsersXml));
+
+            //02. Import Products
+            //var inputProductsXml = File.ReadAllText($"{DatasetsDirectoryPath}/products.xml");
+            //Console.WriteLine(ImportProducts(db, inputProductsXml));
+
+            //03. Import Categories
+            //var inputCategoriesXml = File.ReadAllText($"{DatasetsDirectoryPath}/categories.xml");
+            //Console.WriteLine(ImportCategories(db, inputCategoriesXml));
+
+            //04. Import Categories and Products
+            var inputCategoriesProductsXml = File.ReadAllText($"{DatasetsDirectoryPath}/categories-products.xml");
+            Console.WriteLine(ImportCategoryProducts(db, inputCategoriesProductsXml));
+
+            //05. Export Products In Range
+
+            //06. Export Sold Products
+
+            //07. Export Categories By Products Count
+
+            //08. Export Users and Products
+
+
+            //CleanAndCreateDb(db, inputUsersXml, inputProductsXml, inputCategoriesXml);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            //InitializeMapper();
+
+            var xmlRoot = new XmlRootAttribute("Users");
+            var xmlSerializer = new XmlSerializer(typeof(List<UserDto>), xmlRoot);
+
+            var usersDto = (List<UserDto>)xmlSerializer.Deserialize(new StringReader(inputXml));
+
+            var users = Mapper.Map<List<User>>(usersDto);
+
+            context.Users.AddRange(users);
+
+            return $"Successfully imported {context.SaveChanges()}";
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputXml)
+        {
+            var xmlRoot = new XmlRootAttribute("Products");
+            var xmlSerializer = new XmlSerializer(typeof(List<ProductDto>), xmlRoot);
+
+            var productsDto = (List<ProductDto>)xmlSerializer.Deserialize(new StringReader(inputXml));
+
+            List<Product> productsForJudge = new List<Product>();
+
+            foreach (var product in productsDto)
+            {
+                Product productJudge = new Product()
+                {
+                    Name = product.Name,
+                    Price = product.Price,
+                    SellerId = product.SellerId,
+                    BuyerId = product.BuyerId,
+                };
+
+                productsForJudge.Add(productJudge);
+            }
+
+            //var products = Mapper.Map<List<Product>>(productsDto);  not working in  judge
+
+            context.Products.AddRange(productsForJudge);
+            context.SaveChanges();
+
+            return $"Successfully imported {productsForJudge.Count}";
+        }
+
+        public static string ImportCategories(ProductShopContext context, string inputXml)
+        {
+            //InitializeMapper();
+
+            var xmlRoot = new XmlRootAttribute("Categories");
+            var xmlSerializer = new XmlSerializer(typeof(List<CategoryDto>), xmlRoot);
+
+            var categoriesDto = (List<CategoryDto>)xmlSerializer.Deserialize(new StringReader(inputXml));
+
+            //Not working in judge
+            //var categories = Mapper.Map<List<Category>>(categoriesDto);
+            //context.Categories.AddRange(categories);
+
+            List<Category> categoriesForJudge = new List<Category>();
+
+            foreach (var category in categoriesDto)
+            {
+                Category categoryForJudge = new Category()
+                {
+                    Name = category.Name
+                };
+
+                categoriesForJudge.Add(categoryForJudge);
+            }
+
+            context.Categories.AddRange(categoriesForJudge);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {categoriesForJudge.Count}";
+        }
+
+        public static string ImportCategoryProducts(ProductShopContext context, string inputXml)
+        {
+            //Working
+            //InitializeMapper();
+            //var xmlRoot = new XmlRootAttribute("CategoryProducts");
+            //var xmlSerializer = new XmlSerializer(typeof(List<CategoryProductDto>), xmlRoot);
+
+            //var catProdDto = (List<CategoryProductDto>)xmlSerializer.Deserialize(new StringReader(inputXml));
+
+            //var categoriesProducts = Mapper.Map<List<CategoryProduct>>(catProdDto.Where(cp=>context.Categories.Any(c=>c.Id == cp.CategoryId)&& context.Products.Any(p=>p.Id == cp.ProductId)));
+
+            //context.CategoryProducts.AddRange(categoriesProducts);
+            //context.SaveChanges();
+
+            //return $"Successfully imported {categoriesProducts.Count}";
+
+            // for 100/100 in Judge
+            //InitializeMapper();
+            var xmlRoot = new XmlRootAttribute("CategoryProducts");
+            var xmlSerializer = new XmlSerializer(typeof(List<CategoryProductDto>), xmlRoot);
+
+            var catProdDto = (List<CategoryProductDto>)xmlSerializer.Deserialize(new StringReader(inputXml));
+
+            List<CategoryProduct> categoriesProductsForJudge = new List<CategoryProduct>();
+
+            foreach (var catprod in catProdDto)
+            {
+                CategoryProduct cpForJudge = new CategoryProduct()
+                {
+                    CategoryId = catprod.CategoryId,
+                    ProductId = catprod.ProductId,
+                };
+
+                if (context.Categories.Any(c => c.Id == cpForJudge.CategoryId) && context.Products.Any(p => p.Id == cpForJudge.ProductId))
+                {
+                    categoriesProductsForJudge.Add(cpForJudge);
+                }
+            }
+
+            context.CategoryProducts.AddRange(categoriesProductsForJudge);
+            context.SaveChanges();
+
+            return $"Successfully imported {categoriesProductsForJudge.Count}";
+        }
+
+        public static void ResetDatabese(ProductShopContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+
+        private static void InitializeMapper()
+        {
+            Mapper.Initialize(cfg => { cfg.AddProfile<ProductShopProfile>(); });
+        }
+
+        public static void CleanAndCreateDb(ProductShopContext context, string inputUsersXml, string inputProductsXml, string inputCategoriesXml)
+        {
+            ResetDatabese(context);
+            ImportUsers(context, inputUsersXml);
+            ImportProducts(context, inputProductsXml);
+            ImportCategories(context, inputCategoriesXml);
+        }
+    }
+}
---
+++ 2021 02 Entity Framework Core/2021 03 28 XML Processing Exercise/06. Export Sold Products/ProductShopProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using ProductShop.Dtos.Export;
+using ProductShop.Dtos.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            this.CreateMap<UserDto, User>();
+
+            this.CreateMap<ProductDto, Product>();
+
+            this.CreateMap<CategoryDto, Category>();
+
+            this.CreateMap<CategoryProductDto, CategoryProduct>();
+
+            this.CreateMap<Product, ProductInRangeDto>()
+                .ForMember(p=>p.Buyer, y=>y.MapFrom(s=>s.Buyer.FirstName + " " + s.Buyer.LastName));
+        }
+    }
+}
