fd3579799d108befdfa07cfbe429d1870d615388
Stancho Simeonov <63386963+Simeonov-Stancho@users.noreply.github.com>
2021-10-08 20:19:36 +0300

    JS Advance

--- 2021 09 JS Advance/2021 10 05 DOM Introduction Exercise/09. Number Convertor/solution.js
+++ 2021 09 JS Advance/2021 10 05 DOM Introduction Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,34 @@
 function solve() {
+    const html = {
+        input: document.getElementById("input"),
+        convertTo: document.getElementById("selectMenuTo"),
+        output: document.getElementById("result"),
+        button: document.querySelector("#container > button"),
+    };
 
-    //TODO...
+    const makeOptions = arr =>
+        arr.map(x => {
+            const option = document.createElement("option")
+            x = x.toLowerCase()
+            option.value = x
+            option.text = `${x.charAt(0).toUpperCase()}${x.slice(1)}`
+            return option
+        });
+
+    const print = n => (html.output.value = n);
+    makeOptions(["binary", "hexadecimal"]).forEach(x => html.convertTo.add(x));
+
+    function convert(to, n) {
+        n = Number(n) || 0
+        const formats = {
+            binary: n => (n >>> 0).toString(2),
+            hexadecimal: n => n.toString(16).toLocaleUpperCase(),
+        }
+
+        return formats[to](n)
+    };
+
+    html.button.addEventListener("click", () =>
+        print(convert(html.convertTo.value, html.input.value))
+    );
 }
